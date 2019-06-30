3744c9f6135eda6fbcbda2d54d6e9f74b6b163b4
Leonardo Fleury <fleuryleomoraes@gmail.com>
2019-06-30 14:30:32 -0300

    More pages

--- site/pages/documentos/index.vue
+++ site/pages/documentos/index.vue
@@ -1,3 +1,24 @@
+<template>
+  <v-layout align-center justify-space-around row fill-height>
+    <header>
+      <h2>Documentos</h2>
+    </header>
+    <v-card flat >
+      <v-img src="/images/contract.png" class="img--center" heigth="200px" width="200px"></v-img>
+      <v-card-actions>
+          <v-btn block to="/#" color="secondary" dark>Artigo: Normas ABNT</v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card flat >
+      <v-img src="/images/contract.png" class="img--center" heigth="200px" width="200px"></v-img>
+      <v-card-actions>
+          <v-btn block to="/#" color="secondary" dark>Trabalho: Normas ABNT</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-layout>  
+</template>
+
+
 <script>
 //let gsheet = require('gsheet-web');
 //1dR77VcLpfWH_tmOcB39uI-eoM3xmmSn-XvruebaZJrA gsheet url
--- site/pages/posts/index.vue
+++ site/pages/posts/index.vue
@@ -1,5 +1,21 @@
 <template>
-  
+  <v-layout align-center justify-space-around row fill-height>
+    <header>
+      <h2>Guias</h2>
+    </header>
+    <v-card flat >
+      <v-img src="/images/acessibilidade-icone.png" class="img--center" heigth="200px" width="200px"></v-img>
+      <v-card-actions>
+          <v-btn block to="/#" color="secondary" dark>Boas Práticas para Acessibilidade Digital</v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card flat >
+      <v-img src="/images/acessibilidade-icone.png" class="img--center" heigth="200px" width="200px"></v-img>
+      <v-card-actions>
+          <v-btn block to="/#" color="secondary" dark>O Guia Final em Tecnologias Assistivas</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-layout>  
 </template>
 
 <script>
